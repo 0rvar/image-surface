446a171f134b1c8c74b1ab66a9b918aec63e2802
Orvar Segerström <orvarsegerstrom@gmail.com>
2014-06-23 11:50:03 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-image-surface
-=============
+# Image surface
+Synchronized image display in the browser, using socket.io. Great for showing images on a second screen or projector.
+
+`node index.js` starts a server on localhost:3000. Open the page in a browser to view the surface.
+`scripts/` contains scripts to send images to the server.
+There are scripts to send a screenshot of the current window to the surface.
---
+++ index.js
@@ -0,0 +1,43 @@
+var express    = require('express')
+var bodyParser = require('body-parser')
+var app        = express()
+var http       = require('http').Server(app)
+var io         = require('socket.io')(http)
+
+// Accept POST data
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
+
+// State
+var image = null
+
+app.post('/', function(req, res){
+  console.log("# New image");
+  res.send(null)
+  image = req.body.image
+  io.emit('image', image)
+})
+
+app.post('/clear', function(req, res) {
+  console.log("# Cleared image")
+  image = null
+  io.emit('image', image)
+})
+
+app.get('/', function(req, res){
+  res.sendfile('index.html')
+})
+
+io.on('connection', function(socket){
+  if(image != null) {
+    socket.emit('image', image)
+  }
+
+  console.log('# User connected')
+  socket.on('disconnect', function(){
+    console.log('# User disconnected')
+  })
+})
+
+http.listen(3000, function(){
+  console.log('Listening on *:3000')
+})
